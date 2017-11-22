82ac9efc5a44169206e7c4d8a3890b5729b1e9b0
rommguy <rommguy@gmail.com>
2017-11-22 16:45:50 +0200

    use withState for code reuse

--- src/components/WithBorder.js
+++ src/components/WithBorder.js
@@ -1,36 +1,25 @@
 import './WithBorder.css'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import {withState} from 'recompose'
 
-export class WithBorder extends Component {
+class WithBorderInner extends Component {
   static propTypes = {
-    color: PropTypes.string
-  }
-
-  constructor() {
-    super()
-
-    this.onMouseEnter = this.onMouseEnter.bind(this)
-    this.onMouseLeave = this.onMouseLeave.bind(this)
-    this.state = {mouseOver: false}
-  }
-
-  onMouseEnter() {
-    this.setState({mouseOver: true})
-  }
-
-  onMouseLeave() {
-    this.setState({mouseOver: false})
+    color: PropTypes.string,
+    mouseOver: PropTypes.bool.isRequired,
+    updateMouseState: PropTypes.func.isRequired
   }
 
   render() {
     return (
-      <div className={`with-border ${this.state.mouseOver ? 'mouse-over' : ''}`}
+      <div className={`with-border ${this.props.mouseOver ? 'mouse-over' : ''}`}
            style={{borderColor: this.props.color}}
-           onMouseEnter={this.onMouseEnter}
-           onMouseLeave={this.onMouseLeave}>
+           onMouseEnter={() => this.props.updateMouseState(true)}
+           onMouseLeave={() => this.props.updateMouseState(false)}>
         {this.props.children}
       </div>
     )
   }
 }
+
+export const WithBorder = withState('mouseOver', 'updateMouseState', false)(WithBorderInner)
