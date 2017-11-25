732c7609aed5efcc8ae9143dd8141cfca7975a0d
rommguy <rommguy@gmail.com>
2017-11-25 16:41:48 +0200

    Use a render function to create WithState and make WithBorder stateless

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import {MainView} from './components/MainView'
 import 'react-table/react-table.css'
 
+const usersData = {
+  '0001': {firstName: 'comfort', lastName: 'account', age: 27, status: 'relationshipnail', visits: 59},
+  '0002': {firstName: 'tin', lastName: 'throne', age: 20, status: 'relationship', visits: 53},
+  '0003': {firstName: 'trains', lastName: 'highway', age: 29, status: 'hand', visits: 2},
+  '0004': {firstName: 'difficulty', lastName: 'clock', age: 28, status: 'single', visits: 89},
+  '0005': {firstName: 'window', lastName: 'honey', age: 29, status: 'complicated', visits: 7},
+}
+
 class App extends Component {
   render() {
     return (
@@ -12,7 +20,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <MainView/>
+        <MainView usersData={usersData} />
       </div>
     );
   }
--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,8 +1,8 @@
-import {connect} from 'react-redux'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ReactTable from 'react-table';
 import {WithBorder} from './WithBorder'
+import {getRandomColorStyleValue} from '../utils'
 import {WithBorderHOC} from './WithBorderHOC'
 import {map, identity} from 'lodash/fp'
 import {columns} from '../constants/columns'
@@ -13,7 +13,44 @@ import 'react-tagsinput/react-tagsinput.css'
 
 //const TableWithBorder = WithBorderHOC(ReactTable) // created statically
 
-class InnerMainView extends Component {
+
+class DyanmicWithState extends Component {
+  static propTypes = {
+    initialState: PropTypes.any
+  }
+
+  constructor(props) {
+    super()
+
+    this.state = {myState: props.initialState}
+  }
+
+  updateState = val => this.setState({myState: val})
+
+  render() {
+    return this.props.children(this.state.myState, this.updateState)
+  }
+}
+
+class StatelessWithBorder extends Component {
+  static propTypes = {
+    borderColor: PropTypes.string,
+    replaceColor: PropTypes.func.isRequired
+  }
+
+  render() {
+    return (
+      <div className={`with-border ${this.props.borderColor ? 'mouse-over' : ''}`}
+           style={{borderColor: this.props.borderColor}}
+           onMouseEnter={() => this.props.replaceColor(getRandomColorStyleValue())}
+           onMouseLeave={() => this.props.replaceColor(null)}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export class MainView extends Component {
   static propTypes = {
     userData: PropTypes.object
   }
@@ -42,17 +79,15 @@ class InnerMainView extends Component {
             defaultPageSize={5}/>
         </div>
         <div className="tags-container">
-          <WithBorder>
-              <TagsInput value={this.state.tags} onChange={this.updateTags}/>
-          </WithBorder>
+          <DyanmicWithState initialState={null}>
+            {(borderColor, replaceColor) => (
+              <StatelessWithBorder replaceColor={replaceColor} borderColor={borderColor}>
+                <TagsInput value={this.state.tags} onChange={this.updateTags}/>
+              </StatelessWithBorder>
+            )}
+          </DyanmicWithState>
         </div>
       </div>
     )
   }
 }
-
-
-const mapStateToProps = function (state) {
-  return {userData: state.userData}
-}
-export const MainView = connect(mapStateToProps)(InnerMainView);
